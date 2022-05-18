b37a71251b215287c3efe1d427fa6a5790d440fc
Sergey Goncharuk <fleshed914@gmail.com>
2022-05-18 14:56:05 +0300

    feat: add html & scss structure to smm page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
-
 $(document).ready(function(){
 
 });
 
+
+$(function() {
+
+    let videoPlayBtn = $('.video__play-btn');
+    let videoBg = $('.video__background');
+    let videoYoutube = $('.video__youtube');
+  
+    videoPlayBtn.on('click', function() {
+      let videoSrc = videoYoutube.prop('src');
+  
+      videoYoutube.attr('src', `${videoSrc}&autoplay=1`);
+      videoBg.css('display', 'none');
+  
+      $(".youtube").each(function() {
+        // Based on the YouTube ID, we can easily find the thumbnail image
+        
+  
+        // Overlay the Play icon to make it look like a video player
+        $(this).append($('<div/>', {'class': 'play'}));
+  
+        
+            // Create an iFrame with autoplay set to true
+            var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+            if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
+  
+            // The height and width of the iFrame should be the same as parent
+            var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': '100%', 'height': '100%' })
+  
+            // Replace the YouTube thumbnail with YouTube HTML5 Player
+            $(this).replaceWith(iframe);
+        
+    });
+  });
+  
+  
+  
+    });
